d7163c4316791904a92e2bfb2b70960f1678241a
Koustubh <kulkarni.k2004@gmail.com>
2025-06-08 00:31:21 +0530

    Fix: Remove Sentry and update Next.js config for static export

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import { FaLocationArrow } from "react-icons/fa6";
 import { HiMail, HiPhone } from "react-icons/hi";
-
+import Image from "next/image";
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 
@@ -9,22 +9,23 @@ const Footer = () => {
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
       <div className="relative">
-        <img
+        <Image
           src="/footer-grid.svg"
           alt="footer"
           className="absolute inset-0 w-full h-full object-cover"
+          width={1920}
+          height={1080}
         />
       </div>
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          Let's <span className="text-purple">Connect</span>
+          Let&apos;s <span className="text-purple">Connect</span>
         </h1>
         
         <div className="mt-10 flex flex-col items-center gap-6">
           <div className="flex flex-col items-center gap-2">
             <h2 className="text-2xl font-semibold">Koustubh Kulkarni</h2>
-            
           </div>
 
           <div className="flex flex-col items-center gap-4 mt-4">
@@ -61,7 +62,7 @@ const Footer = () => {
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={info.img} alt="icons" width={20} height={20} />
+              <Image src={info.img} alt="social media icon" width={20} height={20} />
             </div>
           ))}
         </div>
